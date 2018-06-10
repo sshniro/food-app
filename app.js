86fa315288e6df70c18c9ddef43c7ee0192b44d4
nirodev <sshniro@gmail.com>
2018-06-10 07:56:31 +0530

    Reafactoring var to let and const

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,37 +11,6 @@ var distancematrixRouter = require('./routes/distancematrix');
 var geo_helper = require('./geo-helper.js');
 
 var app = express();
-
-var redis = require("redis"),
-    client = redis.createClient();
-
-// if you'd like to select database 3, instead of 0 (default), call
-// client.select(3, function() { /* ... */ });
-
-client.on("error", function (err) {
-    console.log("Error " + err);
-});
-
-var geo = require('georedis').initialize(client);
-
-var locationSet = {
-    'Toronto': {latitude: 43.6667, longitude: -79.4167},
-    'Philadelphia': {latitude: 39.9523, longitude: -75.1638},
-    'Palo Alto': {latitude: 37.4688, longitude: -122.1411},
-    'San Francisco': {latitude: 37.7691, longitude: -122.4449},
-    'St. John\'s': {latitude: 47.5500, longitude: -52.6667},
-    'New York': {latitude: 40.7143, longitude: -74.0060},
-    'Twillingate': {latitude: 49.6500, longitude: -54.7500},
-    'Ottawa': {latitude: 45.4167, longitude: -75.7000},
-    'Calgary': {latitude: 51.0833, longitude: -114.0833},
-    'Mumbai': {latitude: 18.9750, longitude: 72.8258}
-};
-
-// --------------------------------
-// geo.addLocations(locationSet, function (err, reply) {
-//     if (err) console.error(err)
-//     else console.log('added locations:', reply)
-// });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/distancematrix.js
+++ routes/distancematrix.js
@@ -1,16 +1,16 @@
 'use strict';
 
-var express = require('express');
-var sortJsonArray = require('sort-json-array');
-var router = express.Router();
+const express = require('express');
+const sortJsonArray = require('sort-json-array');
+const router = express.Router();
 
-var googleMapsProvider = require('../providers/googleMapsProvider.js');
-var geo_helper = require('../geo-helper.js');
+const googleMapsProvider = require('../providers/googleMapsProvider.js');
+const geo_helper = require('../geo-helper.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-    var driversToDestination = {
+    let driversToDestination = {
         origins : req.query.origins || '40.6905615,-73.9976592|40.6905615,-73.9976592|40.6905615,-73.9976592|40.6905615,-73.9976592|40.6905615,-73.9976592|40.6905615,-73.9976592|40.659569,-73.933783|40.729029,-73.851524|40.6860072,-73.6334271|40.598566,-73.7527626|40.659569,-73.933783|40.729029,-73.851524|40.6860072,-73.6334271|40.598566,-73.7527626',
         destinations : req.query.destinations || '40.6655101,-73.89188969999998'
     };
@@ -25,11 +25,11 @@ router.get('/', function(req, res, next) {
 
 router.get('/drivers', function(req, res, next) {
 
-    var destinationJson = {
+    let destinationJson = {
         destination : req.query.destination || '6.850740,79.873541'
     };
 
-    var destinationSplit = destinationJson.destination.split(',');
+    let destinationSplit = destinationJson.destination.split(',');
 
     geo_helper.getNearByVehicles(destinationSplit[0], destinationSplit[1]).then(function (geoHelperResponse) {
 
