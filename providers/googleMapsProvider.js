c6609f4a07030560209ed82831635186b002047e
System Administrator <root@Nikethans-MacBook-Pro.local>
2018-06-10 08:21:01 +0530

    Added config file for googleAPI

--- providers/googleMapsProvider.js
+++ providers/googleMapsProvider.js
@@ -1,8 +1,8 @@
 'use strict';
 
-
-let express = require('express');
 let request = require('request');
+
+const baseConfig = require('../config/baseConfig.js');
 
 module.exports = {
 	calculateDistance: calculateDistance
@@ -10,27 +10,26 @@ module.exports = {
 
 function calculateDistance(json){
 
-	var options = { 
+	let options = {
 		method: 'GET',
 		url: 'https://maps.googleapis.com/maps/api/distancematrix/json',
 		qs: { 
 			units: 'imperial',
 			origins: json.origins,
 			destinations: json.destinations
+			// , key : baseConfig.googleAPIKEY
 		},
 		headers: {
 			'cache-control': 'no-cache' 
 		} 
 	};
 
-	console.log(options)
-
 	return new Promise(function (resolve, reject) {
 
 		request(options, function (error, response, body) {
 			if (error) return reject(error);
 
-            if (body.status != 'INVALID_REQUEST') {
+            if (body.status !== 'INVALID_REQUEST') {
                 return resolve(body);
             }else{
                 return reject(error);
