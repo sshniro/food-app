ed23287808cb4dfa1f5eb4c06ccb23894262fe7e
System Administrator <root@Nikethans-MacBook-Pro.local>
2018-06-13 18:04:45 +0530

    UI Google maps changes & Drivers API

--- config/baseConfig.js
+++ config/baseConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    googleAPIKEY: 'AIzaSyAmzyTkncIKlTOIEicKEZDIfo395RYDMgU',
+    googleAPIKEY: 'APIKEY',
     driverGroupSegByDistanceVal: 500, // This conf will be used to divide the drivers in to group by distance and sort by rating
     dataaseURL: 'postgres://localhost:5432/nikethanselvanathan'
 };
